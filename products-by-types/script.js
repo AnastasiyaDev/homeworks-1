083744625e9effaf2f8946902c4b82df3b011b94
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-07-08 01:27:35 +0300

    add ajax, checking LoDash work

--- products-by-types/script.js
+++ products-by-types/script.js
@@ -1,1 +1,46 @@
-// Ваш код
+"use strict";
+
+	var sale = [];
+	var promo = [];
+	var recommended = [];
+
+	$.ajax({
+    // url: "https://raw.githubusercontent.com/AnastasiyaDev/homeworks-1/master/products-by-types/products.json",
+    url: 'products.json',
+    dataType : 'json'
+	}).done(function (products) {
+
+		
+	    // console.log(data);
+	    products.forEach(function(item) {
+  			if (item.type == 'sale'){
+  				sale.push(item);
+  			}
+  			else if (item.type == 'promo'){
+  				promo.push(item);
+  			}
+  			else if (item.type == 'recommended'){
+  				recommended.push(item);
+  			}
+		});
+
+	// $('#sale').html( tmpl({products: sale}) );
+
+
+	}).fail(function (xhr, status, errorThrown) {
+	    alert("Извините, произошла ошибка. Пожалуйста, обновите страницу и попробуйте еще раз.");
+	    console.log("Error: " + errorThrown);
+	    console.log("Status: " + status);
+	    console.dir(xhr);
+	}).always(function (xhr, status) {
+	    $.fancybox.hideLoading(); 
+	});
+
+
+	$(document)
+	    .on('ajaxStart', function () {
+	        $.fancybox.showLoading();
+	    })
+	    .on('ajaxStop', function () {
+	        $.fancybox.hideLoading();
+	    });
