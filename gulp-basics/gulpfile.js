eaa8b2f4d85f435444826580490e232e4fac340c
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-07-18 21:16:08 +0300

    add watcher

--- gulp-basics/gulpfile.js
+++ gulp-basics/gulpfile.js
@@ -5,30 +5,28 @@ const less = require('gulp-less');
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const uglify = require('gulp-uglify');
+const minifyCss  = require('gulp-minify-css');
 const debug = require('gulp-debug');
-const sourcemaps = require('gulp-sourcemaps'); // cтруктура даных отражающих что было--что стало, для браузеров
+const sourcemaps = require('gulp-sourcemaps'); 
 const jsObfuscator = require('gulp-js-obfuscator');
 const imagemin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create();
 const eslint = require('gulp-eslint');
-
-
 const gulpIf = require('gulp-if');
 const del = require('del');
 
-const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV == 'development';
-const isProduction = process.env.NODE_ENV == 'production';
- 
-gulp.task('serve', function(){
-	browserSync.init({
-     	server: "./dist"
- 	});
+const isDevelopment = process.env.NODE_ENV == 'development';
+const isProduction = !process.env.NODE_ENV || process.env.NODE_ENV == 'production';
 
- 	browserSync.watch("./src/*.*").on('change', browserSync.reload);
-});
+const paths = {
+    html:['src/**/*.html'],
+    img:['src/img/**/*.*'],
+    less:['src/less/**/*.less'],
+    js:['src/js/**/*.js']
+};
 
 gulp.task('styles', function () {
-	return gulp.src('src/less/**/*.less')  // до * - base, после - relative
+	return gulp.src(paths.less) 
 		// .pipe(debug({titile: 'src'}))
 		.pipe(gulpIf(isProduction, sourcemaps.init()))
 		.pipe(less())
@@ -36,44 +34,61 @@ gulp.task('styles', function () {
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions']
 		 }))
-		.pipe(gulpIf(isProduction, uglify()))
+		.pipe(gulpIf(isProduction, minifyCss()))
 		.pipe(gulpIf(isProduction, sourcemaps.write()))
-		.pipe(gulp.dest('dist/css')); // public+ relative
+		.pipe(gulp.dest('dist/css')) 
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('scripts', function() {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(paths.js)
         .pipe(gulpIf(isProduction, sourcemaps.init()))
         .pipe(concat('all.js'))
         .pipe(gulpIf(isProduction, uglify()))
         .pipe(gulpIf(isProduction, jsObfuscator()))
         .pipe(gulpIf(isProduction, sourcemaps.write()))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('img', function(){
-    return gulp.src('src/img/**')
+gulp.task('img', function() {
+    return gulp.src(paths.img)
     	.pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('entryFiles', function(){
-    return gulp.src(['src/*.html', 'src/*.php'])
+gulp.task('entryFiles', function() {
+    return gulp.src(paths.html)
         .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 gulp.task('eslint', function() {
     return gulp.src('src/js/**/*.js')
-    	.pipe(gulpIf(isProduction, eslint()))
+        .pipe(gulpIf(isProduction, eslint()))
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['styles', 'scripts', 'img', 'entryFiles', 'eslint', 'serve']);
+gulp.task('watcher', function() {
+  gulp.watch(paths.less, ['styles']);
+  gulp.watch(paths.js, ['scripts']);
+  gulp.watch(paths.html, ['entryFiles']);
+});
+
+gulp.task('serve', function() {
+
+    browserSync.init({
+         server: "./dist/"
+     });
+});
+
+
+gulp.task('default', ['img', 'entryFiles', 'styles', 'scripts', 'serve', 'watcher']);
 
 
 // gulp.task('clean', function(){
 // 	return del('dist');
 // });
 
-// gulp.task('watcher', function(){
-// 	gulp.watch('src/less/**/*.less', gulp.series('styles'));
-// });
+
