8d65770bb4721df2e41d4f0d4a3427af940ccb44
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-08-01 09:14:16 +0300

    fix flex

--- responsive_design/src/js/scripts.js
+++ responsive_design/src/js/scripts.js
@@ -3,17 +3,21 @@
 
 $(function() {
 
-    var arr = ['banner_bottom.jpg', 'banner_top.jpg', 'black.jpg', 'blue.jpg', 'gray.jpg', 'red.jpg', 'tile_1.jpg', 'tile_2.jpg'];
+    var imagesList = ['banner_bottom.jpg', 'banner_top.jpg', 'black.jpg', 'blue.jpg', 'gray.jpg', 'red.jpg', 'tile_1.jpg', 'tile_2.jpg'];
+    var menuList = ['MAKEUP', 'SKINCARE', 'FRAGRANCES', 'ARMANI', '|', 'PRAVE', 'ONLINE', 'EXCLUSIVES', 'GIFTS', 'MAESTRO OF DETAILS'];
+    var $headerMenuWide = $('#header-menu-wide');
     var $imgGrid = $('#img-grid');
     var $menuToggle = $('.menu-toggle');
     var $openMenu = $('.menu');
 
     var tmpl = _.template($('#list-template').html());
+    var tmplMenu = _.template($('#list-template-menu').html());
 
-    // $imgGrid.html(tmpl({ images: arr }));
 
+    // $imgGrid.html(tmpl({ images: imagesList }));
+    $headerMenuWide.html(tmplMenu({ items: menuList }));
 
-    $menuToggle.on('click',function(){
+    $menuToggle.on('click', function() {
         $openMenu.toggleClass('open');
     });
 
