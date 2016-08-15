2473c17e2052ad6d7394597f2b3093ebc9568e70
Anastasiya Bakhnar <katana8832@yandex.ru>
2016-08-16 00:59:25 +0300

    improve webpack.config & upgrade package.json

--- responsive_design/src/js/scripts.js
+++ responsive_design/src/js/scripts.js
@@ -3,7 +3,6 @@
 
 $(function() {
 
-    // var imagesList = ['banner_bottom.jpg', 'banner_top.jpg', 'black.jpg', 'blue.jpg', 'gray.jpg', 'red.jpg', 'tile_1.jpg', 'tile_2.jpg'];
     var menuList = ['MAKEUP', 'SKINCARE', 'FRAGRANCES', 'ARMANI', '|', 'PRAVE', 'ONLINE', 'EXCLUSIVES', 'GIFTS', 'MAESTRO OF DETAILS'];
     var $headerMenuWide = $('#header-menu-wide');
     var $headerMenuMob = $('#menuToggle');
--- responsive_design/webpack.config.js
+++ responsive_design/webpack.config.js
@@ -1,23 +1,27 @@
 /*global __dirname process */
-const NODE_ENV = process.env.NODE_ENV  || 'development';
+const NODE_ENV = process.env.NODE_ENV || 'development';
 const webpack = require('webpack');
 
 module.exports = {
     context: __dirname + '/src',
     entry: {
-        home: './home',
-        about: './about'
+        common: './common',
+        header: './header',
+        img_grid: './img_grid',
+        mobile_menu: './mobile_menu',
+        footer: './footer'
     },
 
     output: {
         path: __dirname + '/dist',
-        filename: '[name].js'
+        filename: '[name].js',
+        library: '[name]'
     },
 
     module: {
         loaders: [{
             test: /\.less$/,
-            loader: 'style!css!less'
+            loader: 'style!css!autoprefixer!less'
         }, {
             test: /\.(svg|eot|ttf|woff|png|jpg|gif)/,
             loader: 'file?name=[name].[ext]'
@@ -33,6 +37,7 @@ module.exports = {
     devtool: NODE_ENV == 'development' ? 'cheap-inline-module-source-map' : null,
 
     plugins: [
+        new webpack.NoErrorsPlugin(),
         new webpack.optimize.CommonsChunkPlugin({
             name: 'common'
         }),
