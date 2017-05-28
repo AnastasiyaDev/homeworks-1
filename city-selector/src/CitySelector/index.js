ec06f8626da194de298ce567514801dc8c7ca353
Anastasiya Bakhnar <katana8832@yandex.ru>
2017-05-28 23:32:27 +0300

    Init citySelictor, add ajax

--- city-selector/src/CitySelector/index.js
+++ city-selector/src/CitySelector/index.js
@@ -1,2 +1,102 @@
+'use strict';
+const $ = require('jquery');
+
 require('./style.less');
-// Your code...
+
+module.exports = class CitySelector {
+    constructor(obj) {
+        // this.elementId = obj.elementId;  // не работает
+        this.id = obj.elementId;
+        this.regionsUrl = obj.regionsUrl;
+        this.localitiesUrl = obj.localitiesUrl;
+        this.saveUrl = obj.saveUrl;
+    }
+
+    init() {
+        $('#info').show();
+
+        $('#' + this.id).html($('<button>', {
+            class: 'js-select-region',
+            type: 'button',
+            html: 'Выбрать регион'
+        }));
+    }
+
+    selectRegions() {
+        $.ajax({
+            url: this.regionsUrl,
+            dataType : 'json',
+            beforeSend: function () {
+            }
+        }).done(function (data) {
+            data.forEach(function(item) {
+                $('<ul>', {
+                    class: 'region-list js-region-list',
+                    'data-id': item.id,
+                    'data-active': 0,
+                    type: 'button',
+                    html: item.title
+                }).appendTo('#citySelector');
+            });
+
+            $('.js-select-region').hide();
+        }).fail(function (xhr, status, errorThrown) {
+            alert("Извините, произошла ошибка. Пожалуйста, обновите страницу и попробуйте еще раз.");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        }).always(function (xhr, status) {
+        });
+    }
+
+    selectLocation(idReg, currentItem) {
+        $.ajax({
+            url: this.localitiesUrl + '/' + idReg,
+            dataType: 'json',
+            beforeSend: function () {
+            }
+        }).done(function (data) {
+            $('#regionText').text(data.id);
+
+            if ($(currentItem).data('active') === 0) {
+                for (var key in data.list) {
+                    $('<li>', {
+                        class: 'region-list__item js-region-item',
+                        html: data.list[key]
+                    }).appendTo($(currentItem));
+                }
+            }
+
+            $(currentItem).data('active', 1);
+        }).fail(function (xhr, status, errorThrown) {
+            alert("Извините, произошла ошибка. Пожалуйста, обновите страницу и попробуйте еще раз.");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        }).always(function (xhr, status) {
+        });
+    }
+
+    saveSelect(reg, loc) {
+        $.ajax({
+            url: this.saveUrl,
+            async: false,
+            type: 'POST',
+            dataType: 'json',
+            data: {
+                'region': reg,
+                'locality': loc
+                },
+            beforeSend: function () {
+            }
+        }).done(function (data) {
+            $('body').html('Ваш запрос отправлен: ' + JSON.stringify(data));
+        }).fail(function (xhr, status, errorThrown) {
+            alert('Извините, произошла ошибка. Пожалуйста, обновите страницу и попробуйте еще раз.');
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        }).always(function (xhr, status) {
+        });
+    }
+};
--- city-selector/src/app.js
+++ city-selector/src/app.js
@@ -1,10 +1,57 @@
+'use strict';
+const $ = require('jquery');
 const CitySelector = require('./CitySelector');
 
-/* Пример создания компонента:
-const citySelector = new CitySelector({
-    elementId: 'citySelector',
-    regionsUrl: 'http://localhost:3000/regions',
-    localitiesUrl: 'http://localhost:3000/localities',
-    saveUrl: 'http://localhost:3000/selectedRegions'
+$('#destroyCitySelector').on('click', () => {
+
 });
-*/
+
+$('#createCitySelector').on('click', () => {
+    const citySelector = new CitySelector({
+        elementId: 'citySelector'
+    });
+
+    citySelector.init();
+});
+
+$('#citySelector').on('click', '.js-select-region', () => {
+    const citySelector = new CitySelector({
+        regionsUrl: 'http://localhost:3000/regions',
+    });
+
+    citySelector.selectRegions();
+});
+
+$('#citySelector').on('click', '.js-region-list', (ev) => {
+    const citySelector = new CitySelector({
+        localitiesUrl: 'http://localhost:3000/localities'
+    });
+
+    let currentItem = ev.currentTarget,
+        idReg = $(currentItem).data('id');
+
+    citySelector.selectLocation(idReg, currentItem);
+});
+
+$('#citySelector').on('click', '.js-region-item', (ev) => {
+    let currentItem = ev.currentTarget,
+        cityName = $(currentItem).text();
+
+    $('.js-submit').remove();
+
+    $('#localityText').text(cityName);
+
+    $('<button>', {
+        class: 'js-submit',
+        type: 'submit',
+        html: 'Сохранить'
+    }).appendTo('#citySelector');
+});
+
+$('#citySelector').on('click', '.js-submit', () => {
+    const citySelector = new CitySelector({
+        saveUrl: 'http://localhost:3000/selectedRegions'
+    });
+
+    citySelector.saveSelect($('#regionText').text(), $('#localityText').text());
+});
